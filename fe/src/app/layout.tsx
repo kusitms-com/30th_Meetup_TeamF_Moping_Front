55dd45826413b3ddbb2e927f733b156b18bcb908
choihooo <hochoi8621@naver.com>
2024-11-24 18:30:02 +0900

    [chore] seo 셋업

--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -3,7 +3,21 @@ import { ReactNode } from "react";
 
 export const metadata = {
   title: "Moping!",
-  description: "Moping",
+  description: "모일 때 맵핀, MOPING",
+  keywords:
+    "모핑, 맵핀, moping, 공유지도, 모임, 네이버 지도, 북마크, 맛집, 카페, 데이트",
+  openGraph: {
+    title: "Moping!",
+    description: "모일 때 맵핀, MOPING",
+    url: "https://www.moping.co.kr",
+    images: "/thumbnail.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Moping!",
+    description: "모일 때 맵핀, MOPING",
+    images: "/thumbnail.png",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
